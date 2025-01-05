aab8199e6c7837252afcf13964b4714b5cb68bc2
AHMZ <ahmz.ut@gmail.com>
2025-01-05 15:53:28 +0330

    Some Clearance and modifications on Q4-5 Assignment 7

--- Assignment-7/Q4/Q4.s
+++ Assignment-7/Q4/Q4.s
@@ -1,47 +1,68 @@
-global  asm_main
-extern  scanf
-extern  printf
+;==============================================
+    ; Assignment 7 - Question 4
+    ; Description: Check if two strings are the same
+    ; AmirHossein MohammadZadeh - 402106434
+    ; MohammadMahdi Mohammadi - 402106456
+;==============================================
 
-section .text
-asm_main:
+%macro enter 1
+    push rbp
+    mov rbp, rsp
+    sub rsp, %1*16
+%endmacro
 
-    sub rsp,    8
-    lea rsi,    [input1]
-    lea rdi,    [scan]
-    call scanf
-    lea rsi,    [input2]
-    lea rdi,    [scan]
-    call scanf
-    lea rsi, [input1]
-    lea rdi, [input2]
-
-compare:
-    mov al, [rsi]     ; Load a byte
-    mov bl, [rdi]
-    cmp al, bl
-    jne not_equal
-    cmp al, 0         ; Check if end of string
-    je  equal         ; both null -> strings are equal
-    inc rsi           ; next byte
-    inc rdi
-    jmp compare       ; Repeat the comparison
-
-not_equal:
-    lea rdi,    [no]
-    call    printf
-    add rsp,    8
+%macro leave 1
+    add rsp, %1*16
+    leave
     ret
+%endmacro
 
-equal:
-    lea rdi,    [yes]
-    call    printf
-    add rsp,    8     
-    ret
+section .bss
+    input1 resb 1000
+    input2 resb 1000
 
 section .data
+    scan:   db  "%s",   0 
+    yes:    db  "input1 and input2 are the same!",  10, 0
+    no:     db  "input1 and input2 are not the same!",  10, 0
 
-input1: db  1000    dup(0)
-input2: db  1000    dup(0)
-scan:   db  "%s",   0 
-yes:    db  "input1 and input2 are the same!",  10, 0
-no:     db  "input1 and input2 are not the same!",  10, 0
+section .text
+global main
+extern printf
+extern scanf
+
+main:
+    enter 0
+    lea rsi, [input1]           ; Load the address of the string
+    lea rdi, [scan]             ; Load the address of the format string
+    call scanf                  ; Call scanf
+
+    lea rsi,    [input2]        ; Load the address of the string
+    lea rdi,    [scan]          ; Load the address of the format string
+    call scanf                  ; Call scanf
+
+    lea rsi, [input1]           ; Load the address of the input1 into rsi
+    lea rdi, [input2]           ; Load the address of the input2 into rdi
+
+compare:
+    mov al, [rsi]               ; Load the byte from the input1 into al
+    mov bl, [rdi]               ; Load the byte from the input2 into bl
+    cmp al, bl                  ; Compare the two bytes
+    jne not_equal               ; If they are not equal, jump to not_equal
+    cmp al, 0                   ; Check if we reached the end of both strings
+    je  equal                   ; if yes, both null -> strings are equal
+    inc rsi                     ; next byte in input1
+    inc rdi                     ; next byte in input2
+    jmp compare                 ; Repeat the comparison for the next byte
+
+not_equal:
+    lea rdi, [no]               ; Load the address of the no string into rdi
+    call printf                 ; Call printf
+    jmp end                     ; Jump to the end
+
+equal:
+    lea rdi, [yes]              ; Load the address of the yes string into rdi
+    call printf                 ; Call printf
+
+end:
+    leave 0
--- Assignment-7/Q5/Q5.s
+++ Assignment-7/Q5/Q5.s
@@ -1,57 +1,68 @@
-;; Assignment 7 - Practical Question 5; 402106434, 402106456
-global  asm_main
-extern  scanf
-extern  printf
+;==============================================
+    ; Assignment 7 - Question 5
+    ; Description: This program counts the number of 0s and 1s in the input string.
+    ; AmirHossein MohammadZadeh - 402106434
+    ; MohammadMahdi Mohammadi - 402106456
+;==============================================
 
-section .text
-asm_main:
-    sub rsp,    8
+%macro enter 1
+    push rbp
+    mov rbp, rsp
+    sub rsp, %1*16
+%endmacro
 
-while:
-    mov rdi,    scan_char
-    mov rsi,    chr
-    call    scanf
-    cmp byte    [chr],  10
-    je  end_while
-    cmp byte    [chr],  '1'
-    je  else
-    cmp byte    [chr],  '0'
-    je  then
-    jmp while
-then:
-    mov rbx,    [zero]
-    inc rbx
-    mov [zero], rbx
-    jmp while
-else:
-    mov rbx,    [one]
-    inc rbx
-    mov [one],  rbx
-    jmp while
-end_while:
-    mov rsi,    num_of_zeros
-    mov rdi,    print_str
-    call    printf
-    mov rsi,    [zero]
-    mov rdi,    print_int
-    call    printf
-    mov rsi,    num_of_ones
-    mov rdi,    print_str
-    call    printf
-    mov rsi,    [one]
-    mov rdi,    print_int
-    call    printf
-    add rsp,    8
+%macro leave 1
+    add rsp, %1*16
+    leave
     ret
+%endmacro
+
+section .bss
+    buffer: resb 100
 
 section .data
-scan_char:  db  "%c",   0
-print_str:  db  "%s",   0
-print_int:  db  "%d",   10, 0
-num_of_ones:    db  "Number of ones: ", 0
-num_of_zeros:   db  "Number of zeros: ", 0
-newline:    db  10, 0
-chr:    db  0
-zero:   dd  0
-one:    dd  0
+    scanf_format: db  "%s",   0
+    num_of_ones:  db  "Number of ones: %d", 10, 0
+    num_of_zeros: db  "Number of zeros: %d", 10, 0
 
+section .text
+global main
+extern printf
+extern scanf
+
+main:
+    enter 1                             ; allocate space for 2 (2*8byte) local variable on the stack
+    mov qword [rsp], 0                  ; initialize the first local variable to 0 (number of 0s)
+    mov qword [rsp + 8], 0              ; initialize the second local variable to 0 (number of 1s)
+
+    lea rdi, [scanf_format]             ; load the address of the format string into rdi
+    lea rsi, [buffer]                   ; load the address of the buffer into rsi
+    call scanf                          ; call scanf to read the input string
+
+    lea rcx, [buffer]                   ; load the address of the buffer into rcx
+_loop:
+    movzx rax, byte [rcx]               ; move the first byte of the buffer into rax
+    cmp rax, '0'                        ; test if rax is zero
+    je _zero                            ; if it is, jump to _zero
+    cmp rax, '1'                        ; test if rax is one
+    je _one                             ; if it is, jump to _one
+    jmp _next                           ; jump to _next
+_zero:
+    inc qword[rsp]                      ; increment the number of 0s
+    jmp _next                           ; jump to _next
+_one:
+    inc qword[rsp + 8]                  ; increment the number of 1s
+_next:
+    inc rcx                             ; move to the next byte
+    cmp byte [rcx], 0                   ; compare the first byte of the buffer with 0
+    jne _loop                           ; if it is not zero, jump to _loop
+
+    lea rdi, [num_of_zeros]             ; load the address of the format string into rdi
+    mov rsi, [rsp]                      ; load the number of 0s into rsi
+    call printf                         ; call printf to print the number of 0s
+
+    lea rdi, [num_of_ones]              ; load the address of the format string into rdi
+    mov rsi, [rsp + 8]                  ; load the number of 1s into rsi
+    call printf                         ; call printf to print the number of 1s
+
+    leave 1
