ffa47072b94b719ee57086603c43e683d6ef8e23
AHMZ <ahmz.ut@gmail.com>
2025-01-05 20:17:22 +0330

    Rewrite the A7-Q1 and fix the bug, fix the sample output

--- Assignment-7/Q1/Q1.s
+++ Assignment-7/Q1/Q1.s
@@ -1,262 +1,239 @@
-;; Assignment 7 - Practical Question 5; 402106434, 402106456
-global  asm_main
-extern  scanf
-extern  printf
-extern  strcmp
-section .text
+;==============================================
+    ; Assignment 7 - Question 1
+    ; Description: Apply some operations on an array of short integers
+    ; AmirHossein MohammadZadeh - 402106434
+    ; MohammadMahdi Mohammadi - 402106456
+;==============================================
 
-%macro  printShort 1
-    movsx   rsi,    word %1
-    mov     rdi,    print_short
-    call    printf
+%macro enter 1
+    push rbp
+    mov rbp, rsp
+    sub rsp, %1*16
 %endmacro
 
-%macro  printInt 1
-    mov     rsi,    %1
-    mov     rdi,    print_int
-    call    printf
-%endmacro
-
-%macro  scanInt 0
-    mov rdi,    scan_int
-    call    scanf
-%endmacro
-
-%macro  scanShort 0
-    mov rdi,    scan_short
-    call    scanf
-%endmacro
-asm_main:
-
-    sub rsp,    8
-    lea rsi,    [input]
-    scanShort
-    lea rsi,    [input+2]
-    scanShort
-    lea rsi,    [input+4]
-    scanShort
-    lea rsi,    [input+6]
-    scanShort
-
-while:
-    lea rsi,    [str]
-    lea rdi,    [scan_str]
-    call scanf
-    lea rdi,    [end]
-    lea rsi,    [str]
-    call strcmp
-    cmp rax,    0
-    je  end_while
-
-
-    lea rdi,    [cmp]
-    lea rsi,    [str]
-    call strcmp
-    cmp rax,    0
-    je  compare
-    
-    lea rdi,    [swap]
-    lea rsi,    [str]
-    call strcmp
-    cmp rax,    0
-    je  swapping
-
-    lea rdi,    [msb]
-    lea rsi,    [str]
-    call strcmp
-    cmp rax,    0
-    je  MSB
-
-
-    lea rdi,    [lsb]
-    lea rsi,    [str]
-    call strcmp
-    cmp rax,    0
-    je  LSB
-
-
-    lea rdi,    [overflow]
-    lea rsi,    [str]
-    call strcmp
-    cmp rax,    0
-    je  flow
-
-    lea rdi,    [div]
-    lea rsi,    [str]
-    call strcmp
-    cmp rax,    0
-    je  division
-
-    lea rdi,    [mul]
-    lea rsi,    [str]
-    call strcmp
-    cmp rax,    0
-    je  multiply
-
-    jmp while
-    
-end_while:
-    add rsp,    8
+%macro leave 1
+    add rsp, %1*16
+    leave
     ret
+%endmacro
 
-compare:
-    lea rsi,    [num]
-    scanInt
-    lea rsi,    [num+4]
-    scanInt
-    mov eax,    [num]
-    shl eax, 1
-    mov ebx,    [num+4]
-    shl ebx, 1
-    mov ax,    [input + eax]
-    mov bx,    [input + ebx]
-    cmp ax,    bx
-    jg  .num1
-    jmp .num2
-.num1:
-    printShort  ax
-    jmp while
-.num2:
-    printShort  bx
-    jmp while
+%macro strcheck 3
+    lea rdi, [%2]                           ; Load the address of the desired command into rdi
+    lea rsi, [%1]                           ; Load the address of the command buffer into rsi
+    call strcmp                             ; Call strcmp to compare the command with the desired command
+    cmp rax, 0                              ; Check if the command is equal to the desired command
+    je %3                                   ; Jump to the desired label if the command is equal to the desired command
+%endmacro
 
-swapping:
-    lea rsi,    [num]
-    scanInt
-    lea rsi,    [num+4]
-    scanInt
-    mov r8d,    [num]
-    shl r8d, 1
-    mov r9d,    [num+4]
-    shl r9d, 1
-    mov ax,    [input + r8d]
-    mov bx,    [input + r9d]
-    mov [input + r8d],   bx
-    mov [input + r9d],   ax
-    jmp while
+%macro strncheck 3
+    lea rdi, [%2]                           ; Load the address of the desired command into rdi
+    call strlen                             ; Call strlen to get the length of the desired command
+    lea rdi, [%1]                           ; Load the address of the command buffer into rsi
+    lea rsi, [%2]                           ; Load the address of the command string into rdi
+    mov rdx, rax                            ; Load the length of the desired command into rdx
+    call strncmp                            ; Call strncmp to compare the command with the desired command
+    cmp rax, 0                              ; Check if the command is equal to the desired command
+    je %3                                   ; Jump to the desired label if the command is equal to the desired command
+%endmacro
 
-MSB:
-    lea rsi,    [num]
-    scanInt
-    mov eax,     [num]
-    shl eax,     1
-    mov ax,     [input + eax]
-    mov bx,     -1
-.msb_loop:
-    cmp ax, 0
-    je  .end_msb
-    shr ax, 1
-    inc bx
-    jmp .msb_loop
-.end_msb:
-    printShort  bx
-    jmp while
+%macro sscan2number 3
+    sub rsp, 16                             ; Allocate space for two numbers
+    lea rdi, [%1]                           ;|
+    lea rsi, [two_num_format]               ;|
+    lea rdx, [rsp]                          ;|
+    lea rcx, [rsp + 4]                      ;|
+    call sscanf                             ;| sscanf(%1, "%d %d", &num1, &num2);
+    mov %2, dword [rsp]                     ;|
+    mov %3, dword [rsp + 4]                 ;|
+    add rsp, 16                             ; Free the allocated space
+%endmacro
 
-LSB:
-    lea rsi,    [num]
-    scanInt
-    mov eax,     [num]
-    shl eax,     1
-    mov ax,     [input + eax]
-    mov bx,     0
-.lsb_loop:
-    movsx r8,    ax
-    and r8,    1
-    cmp r8,    1
-    je  .lsb_end
-    shr ax, 1
-    inc bx
-    jmp .lsb_loop
-.lsb_end:
-    printShort  bx
-    jmp while
+%macro sscannumber 2
+    sub rsp, 16                             ; Allocate space local
+    lea rdi, [%1]                           ;|
+    lea rsi, [num_scan_fmt]                 ;|
+    lea rdx, [rsp]                          ;|
+    call sscanf                             ;| sscanf(%1, "%d", &num);
+    mov %2, dword [rsp]                     ;|
+    add rsp, 16                             ; Free the allocated space
+%endmacro
 
+%macro printnumber 1
+    lea rdi, [num_print_fmt]                ; Load the address of the number format string into rdi
+    mov rsi, %1                             ; Load the number into rsi
+    call printf                             ; Call printf to print the number
+%endmacro
 
-flow:
-    lea rsi,    [num]
-    scanInt
-    lea rsi,    [num+4]
-    scanInt
-    mov eax,    [num]
-    shl eax, 1
-    mov ebx,    [num+4]
-    shl ebx, 1
-    mov ax,    [input + eax]
-    mov bx,    [input + ebx]
-    add ax,    bx
-    jo  .overflow
-    lea rsi,    [no]
-    lea rdi,    [print_str]
-    call    printf
-    jmp while
-.overflow:
-    lea rsi,    [yes]
-    lea rdi,    [print_str]
-    call    printf
-    jmp while
+%macro safepush 1
+    sub rsp, 16
+    mov [rsp], %1
+%endmacro
 
+%macro safepop 1
+    mov %1, [rsp]
+    add rsp, 16
+%endmacro
 
-division:
-    lea rsi,    [num]
-    scanInt
-    lea rsi,    [num+4]
-    scanInt
-    mov eax,    [num]
-    shl eax, 1
-    mov ebx,    [num+4]
-    shl ebx, 1
-    mov bx,    [input + ebx]
-    cmp bx,    0
-    je  .invalid
-    mov ax,    [input + eax]
-    cwd
-    idiv bx
-    mov bx,    dx
-    printShort  ax
-    printShort  bx
-    jmp while
-.invalid:
-    lea rsi,    [invalid]
-    lea rdi,    [print_str]
-    call    printf
-    jmp while
+section .bss
+    command:        resb    1000
 
-multiply:
-    lea rsi,    [num]
-    scanInt
-    lea rsi,    [num+4]
-    scanInt
-    mov eax,    [num]
-    shl eax, 1
-    mov ebx,    [num+4]
-    shl ebx, 1
-    mov ax,    [input + eax]
-    mov bx,    [input + ebx]
-    imul bx
-    movsx rax,   ax
-    printInt    rax
-    jmp while
+section .rodata
+    input_format:   db  "%hd %hd %hd %hd", 0
+    read_line_fmt:  db  "%[^", 10, "]",    0
+    num_scan_fmt:   db  "%d",     0
+    num_print_fmt:  db  "%d",     10, 0
+    two_num_format: db  "%d %d",  0
+    cmp_format:     db  "cmp ",      0
+    swap_format:    db  "swap ",     0
+    mul_format:     db  "mul ",      0
+    div_format:     db  "div ",      0
+    msb_format:     db  "msb ",      0
+    lsb_format:     db  "lsb ",      0
+    overflow_format:db  "overflow ", 0
+    exit_format:    db  "exit",      0
+    yes_format:     db  "YES", 10, 0
+    no_format:      db  "NO",  10, 0
+    invalid_fmt:    db  "Invalid division!", 10, 0
+    error_msg:      db  "Invalid command!", 10, 0
 
 section .data
+    input:          dw 0, 0, 0, 0
 
-scan_int:       db  "%d",   0
-scan_short:     db  "%hd",  0
-print_short:    db  "%hd",  10, 0
-print_str:      db  "%s",   10, 0
-print_int:      db  "%d",   10, 0
-input:          dw  0,  0,  0,  0
-num:            dd  0,  0
-str:            db  1000    dup(0)
-scan_str:       db  "%s",   0
-cmp:            db  "cmp",  0
-swap:           db  "swap", 0
-mul:            db  "mul",  0
-div:            db  "div",  0
-msb:            db  "msb",  0
-lsb:            db  "lsb",  0
-overflow:       db  "overflow", 0
-carry:          db  "carry", 0
-end:            db  "end", 0
-yes:            db  "YES", 0
-no:             db  "NO",  0
-invalid:        db  "Invalid division!", 0
+section .text
+global main
+extern printf
+extern scanf
+extern strlen
+extern strncmp
+extern sscanf
+extern stdin
+extern getchar
+extern strcmp
+
+;============================== main() ===================================
+main:
+    enter 0
+
+    lea rdi, [input_format]                 ; Load the address of the input format string into rdi
+    lea rsi, [input]                        ; Load the address of the first element of the input array into rsi
+    lea rdx, [input + 2]                    ; Load the address of the second element of the input array into rdx
+    lea rcx, [input + 4]                    ; Load the address of the third element of the input array into rcx
+    lea r8, [input + 6]                    ; Load the address of the fourth element of the input array into r8
+    call scanf                              ; Call scanf to read the input values
+    call getchar                            ; Read the newline character
+
+_main_loop:
+    ; Read the command with fgets
+    lea rdi, [read_line_fmt]                ; Load the address of the read line format string into rdi
+    lea rsi, [command]                      ; Load the address of the command buffer into rdi
+    call scanf                              ; Call scanf to read the command
+    call getchar                            ; Read the newline character
+
+    ; Check if the command is "exit"
+    strcheck command, exit_format, _exit
+
+    ; Check if the command is "cmp "
+    strncheck command, cmp_format, _compare
+
+    ; Check if the command is "swap "
+    strncheck command, swap_format, _swap
+
+    ; Check if the command is "mul "
+    strncheck command, mul_format, _multiply
+
+    ; Check if the command is "div "
+    strncheck command, div_format, _divide
+
+    ; Check if the command is "msb "
+    strncheck command, msb_format, _msb_index
+
+    ; Check if the command is "lsb "
+    strncheck command, lsb_format, _lsb_index
+
+    ; Check if the command is "overflow "
+    strncheck command, overflow_format, _overflow
+
+    ; If the command is not valid, print an error message
+    lea rdi, [error_msg]                    ; Load the address of the error message into rsi
+    call printf                             ; Call printf to print the error message
+    jmp _main_loop                          ; Jump to the main loop
+
+_compare:
+    sscan2number command + 4, eax, ebx      ; Parse the indexes from the command and store them in rax and rbx
+    mov ax, word [input + eax*2]            ; Load the first number into ax
+    mov si, word [input + ebx*2]            ; Load the second number into si
+    cmp ax, si                              ; Compare the two numbers
+    jge .num1                               ; If value1 >= value2, jump to num1
+    mov ax, si                              ; Otherwise, move value2 to ax
+.num1:
+    movsx rax, ax                           ; Zero-extend the result
+    printnumber rax                         ; Print the result of the comparison
+    jmp _main_loop                          ; Jump to the main loop
+
+_swap:
+    sscan2number command + 5, eax, ebx      ; Parse the indexes from the command and store them in rax and rbx
+    mov di, word [input + eax*2]            ; Load the first number into rdi
+    mov si, word [input + ebx*2]            ; Load the second number into rsi
+    mov word [input + eax*2], si            ; Swap the two numbers
+    mov word [input + ebx*2], di            ; Swap the two numbers
+    jmp _main_loop                          ; Jump to the main loop
+
+_multiply:
+    sscan2number command + 4, eax, ebx      ; Parse the indexes from the command and store them in rax and rbx
+    mov ax, word [input + eax*2]            ; Load the first number into ax
+    imul word [input + ebx*2]               ; Multiply the first number by the second number
+    movsx rax, ax                           ; Sign-extend the result
+    printnumber rax                         ; Print the result of the multiplication
+    jmp _main_loop                          ; Jump to the main loop
+
+_divide:
+    sscan2number command + 4, eax, ebx      ; Parse the indexes from the command and store them in rax and rbx
+    mov ax, word [input + eax*2]            ; Load the first number into rdi
+    mov bx, word [input + ebx*2]            ; Load the second number into rsi
+    movsx rax, ax                           ; Sign-extend the numbers
+    movsx rbx, bx                           ; Sign-extend the numbers
+    cmp rbx, 0                              ; Check if the second number is zero
+    je _invalid                             ; Jump to the invalid label if the second number is zero
+    cqo                                     ; Sign-extend rax into rdx:rax
+    idiv rbx                                ; Divide the rdx:rax by the rbx
+    safepush rdx                            ; Save the remainder
+    printnumber rax                         ; Print the quotient
+    safepop rdx                             ; Restore the remainder
+    printnumber rdx                         ; Print the remainder
+    jmp _main_loop                          ; Jump to the main loop
+_invalid:
+    lea rdi, [invalid_fmt]                  ; Load the address of the "Invalid division!" string into rdi
+    call printf                             ; Print the error message
+    jmp _main_loop                          ; Jump to the main loop
+
+_msb_index:
+    sscannumber command + 4, eax            ; Parse the index from the command and store it in rax
+    bsr ax, word [input + eax*2]            ; Determine the index of the most significant bit
+    movzx rax, ax                           ; Zero-extend the result
+    printnumber rax                         ; Print the index of the most significant bit
+    jmp _main_loop                          ; Jump to the main loop
+
+_lsb_index:
+    sscannumber command + 4, eax            ; Parse the index from the command and store it in rax
+    bsf ax, word [input + eax*2]            ; Load the number into rdi
+    movzx rax, ax                           ; Zero-extend the result
+    printnumber rax                         ; Print the index of the least significant "one" bit
+    jmp _main_loop                          ; Jump to the main loop
+
+_overflow:
+    sscan2number command + 9, eax, ebx      ; Parse the indexes from the command and store them in rax and rbx
+    mov di, word [input + eax*2]            ; Load the first number into rdi
+    mov si, word [input + ebx*2]            ; Load the second number into rsi
+    add di, si                              ; Add the two numbers
+    jo .overflow_yes                        ; Jump to the overflow_yes label if the result is not zero
+    lea rdi, [no_format]                    ; Load the address of the "NO" string into rsi
+    jmp .overflow_end
+.overflow_yes:
+    lea rdi, [yes_format]                   ; Load the address of the "YES" string into rsi
+.overflow_end:
+    call printf                             ; Print the result of the overflow check
+    jmp _main_loop                          ; Jump to the main loop
+
+_exit:
+    leave 1
+
